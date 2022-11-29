7c9d2cd225b1b4ec3dcc45eb2287c44f6c6f983e
letusbeus <letusbeus@mail.ru>
2022-11-30 02:24:21 +0300

    Homework 2: task 2

--- src/ru/letusbeus/ExceptionsHomeworkTwo/Main.java
+++ src/ru/letusbeus/ExceptionsHomeworkTwo/Main.java
@@ -1,9 +1,11 @@
 package ru.letusbeus.ExceptionsHomeworkTwo;
 
 import static ru.letusbeus.ExceptionsHomeworkTwo.TaskOne.checkIfUserNumberIsFloat;
+import static ru.letusbeus.ExceptionsHomeworkTwo.TaskTwo.divideArrayNums;
 
 public class Main {
     public static void main(String[] args) {
         checkIfUserNumberIsFloat();
+        divideArrayNums();
     }
 }
