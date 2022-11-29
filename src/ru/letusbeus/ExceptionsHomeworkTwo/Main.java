86ce384a84bd5d36cc0fb33bfe6d4ec085b5eedd
letusbeus <letusbeus@mail.ru>
2022-11-29 21:13:31 +0300

    Homework 2: task 1

---
+++ src/ru/letusbeus/ExceptionsHomeworkTwo/Main.java
@@ -0,0 +1,9 @@
+package ru.letusbeus.ExceptionsHomeworkTwo;
+
+import static ru.letusbeus.ExceptionsHomeworkTwo.TaskOne.checkIfUserNumberIsFloat;
+
+public class Main {
+    public static void main(String[] args) {
+        checkIfUserNumberIsFloat();
+    }
+}
---
+++ src/ru/letusbeus/ExceptionsHomeworkTwo/TaskOne.java
@@ -0,0 +1,25 @@
+package ru.letusbeus.ExceptionsHomeworkTwo;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+// Реализуйте метод, который запрашивает у пользователя ввод дробного числа (типа float),
+// и возвращает введенное значение. Ввод текста вместо числа не должно приводить к падению приложения,
+// вместо этого, необходимо повторно запросить у пользователя ввод данных.
+
+public class TaskOne {
+    public static void checkIfUserNumberIsFloat() {
+        while (true) {
+            System.out.println("Введите дробное число:");
+            Scanner sc = new Scanner(System.in);
+
+            try {
+                float userFloat = sc.nextFloat();
+                System.out.println(userFloat);
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Введенное значение не является дробным числом, пожалуйста, повторите ввод:");
+            }
+        }
+    }
+}
